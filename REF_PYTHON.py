da99290c72ef49ba187642478422d4abc5ee38a8
FiratErman <firat.erman@outlook.fr>
2021-05-03 11:39:41 +0200

    ok

---
+++ REF_PYTHON.py
@@ -0,0 +1,110 @@
+### PYTHON CONCEPTUAL HIERARCHY
+# Python programs are decomposed: 
+#- modules > statements > expressions > objects(it is data structure)
+
+### STRING OPERATIONS
+>>>S='SPAM'
+>>>S.find("pa")
+
+>>>S.replace('pa','XYZ') 
+>>>S[1:3]="XYZ"
+
+###SPLIT
+>>> line= 'aaa,bbb,cc'
+>>> line.split(',')
+
+
+### NUMBER OPERATIONS
+>>> s=[1,3,4]
+>>> s[0]=2
+
+
+### FORMATTING 
+>>>'{}, eggs, and {}'.format('spam','SPAM!')
+ 
+ >>>'%.2f | %+05d' % (3.14159, −42)
+
+
+#Other Ways to Code Strings
+>>>S="A\nB\tC"
+>>>len(S)
+#n for going to the line and \t for tab
+
+### PATTERN MATCHING 
+# module re calls for searching, splitting and replacement. 
+>>> import re
+>>> match = re.match('Hello[ \t]*(.*)world', 'Hello Python world')
+>>> match.group(1)
+#This example searches for a substring that begins with the word “Hello,” followed by
+#zero or more tabs or spaces, followed by arbitrary characters to be saved as a matched
+#group, terminated by the word “world.” 
+
+#split
+>>>re.split('l','Hello world')
+
+
+###LISTS
+# list object are ordrederd collections of typed objects= 
+    - no fix size 
+    - mutable (can be modified)
+
+L = [123, 'spam', 1.23]
+#to add
+>>>>>L + [4, 5, 6] 
+>>>L.append(2)
+#To sort only same types of objects (numerical or string)
+>>> L.sort
+
+
+#TYPE-SPECIFIC OPERATIONS
+>>>L.append('NI')
+
+
+###LIST COMPREHENSION EXPRESSION
+#powerful way to process structurs
+#There are a way to build a new list by running an axpression on ach item in a sequecnce
+# one at a time from left to right
+>>> M=[[1,2,3],[4,5,6],[7,8,9]]
+>>> col2=[row[1] for row in M] # Give me row[1] for each row in matrix M in a new list 
+>>> diag= [M[i][i] for i in [0,1,2]]
+>>> doubles = [c*2 for c in 'spam']
+>>> [[x ** 2, x ** 3] for x in range(4)]
+
+>>> {sum(row) for row in M}
+
+>>> squares = [x ** 2 for x in [1, 2, 3, 4, 5]]
+>>> squares
+[1, 4, 9, 16, 25]
+
+>>> squares = []
+>>>for x in [1, 2, 3, 4, 5]:
+    squares.append(x ** 2)
+>>> squares
+[1, 4, 9, 16, 25]
+
+
+
+
+### DICTIONARIES
+#Dictionaires are different, are not sequences and are instead known as mappings.
+#They store object by KEY instead of positions.
+#MUTABLE like lists they may be changed in place and can grow and shrink on demand
+>>> D = {'food': 'Spam', 'quantity': 4, 'color': 'pink'}
+>>> D['food']
+'Spam'
+D['quantity'] += 1
+>>> D
+{'color': 'pink', 'food': 'Spam', 'quantity': 5}
+#with an empty dictionary
+>>>D = {}
+>>> D['name'] = 'Bob'
+>>>D['job'] = 'dev'
+>>> D['age'] = 40
+# Create keys by assignment
+>>> D
+{'age': 40, 'job': 'dev', 'name': 'Bob'}
+#dictonaries are NOT sequences, keys may come back in a different order than in which we typed them
+
+
+###TUPLES
+#Tuples are sequences like list but are IMMUTABLE
---
+++ unix_workbench.sh
@@ -0,0 +1,111 @@
+
+Sudo = Super User Do
+su ferman # to connect to ferman 
+
+
+
+
+## the root : / 
+## the home directory: ~ 
+
+
+# To know where you are
+PWD 
+
+
+# look at the list 
+LS 
+
+#look at the list and get details of the files 
+    -ls -l 
+
+
+# wildcard * is a sign used to find similar files
+ls ba* #to find every folders starting with ba
+ls *ba* 
+
+
+#Change repertory to      
+CD 
+cd + letter + (click on tab)
+
+# to return to the precedent repertory
+CD ..  
+
+#to create a directory
+MKDIR namedirectory 
+
+
+#To create a focument
+TOUCH name.txt 
+
+# to display the number of line in a file follow by number of words
+wc name.txt 
+
+# use to print text file on the terminal
+# can also concatenate 
+cat name.txt
+cat name.txt name.txt pour concatener 
+
+#to display the first line and last line of a file 
+HEAD 
+TAIL 
+HEAD -n 10 
+to display the first line and last line of a file 
+head name.txt
+head -n 4 
+the n option followed by the number of line you want to see 
+
+#to print something OR to append something in a document
+
+echo "My name is Firat" 
+echo "My name is Firat" > name.txt 
+#or add something in the last of a file 
+    echo "My name is Firat" >> name.txt 
+
+
+NANO 
+Text editor
+nano name.txt 
+
+
+
+# MIGRATION + DESTRUCTION 
+#to move file or to rename file 
+MV 
+
+# move my journal entry journal-2017-01-24.txt into the Journal directory
+mv journal-2017-01-24.txt Journal
+#move the Journal directory into the Documents folder
+mv Journal Documents
+
+mv file* folder/ 
+
+# to copy file 
+cp echo-out.txt Desktop
+    #Be aware that there is one difference between copying files
+    #and folders, when copying folders you need to specify the -r
+    #option :
+    cp -r Documents Desktop
+
+cp /fd/fdf/  .   #the point means this current directory
+#To drop documents and file  
+RM 
+rm -r Desktop/Documents to drop directory
+
+
+###Difference between ECHO and EXPORT
+>>> vech=bus
+>>> echo "$vech"
+bus
+>>> bash #to start a new shell instance
+>>> echo "$vech"
+output is empty
+#Empty line as the variable vech is not exported to new process. 
+#But to make the variable known to child process use the export command.
+>>> export vech="bus"
+>>> echo $backup
+bus
+>>> bash 
+>>> echo $backup 
+bus 
